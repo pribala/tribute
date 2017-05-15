e4e480464c624ecb27b93b8909ec8c0dbcdd2f7f
PriBala <andu_pri@yahoo.com>
2017-05-15 10:45:43 -0400

    feat: Update dice program
    Update dice program to include user input.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
+
 app = Flask(__name__)
 import json
 import random
@@ -11,29 +13,29 @@ def hello_world():
 def diceRoll():
     if request.method == 'POST':
     # num = int(input('How many rolls do you want to simulate? '))
-        num = request.form['rolls']	
-		result = {} 
-		rolls = {}
-		for k in range(2, 13):
-			rolls[k] = 0
-		doubles = 0
+        # Get the value for number of rolls entered by user	
+        num = int(request.form['rolls'])	
+        result = {}   # create a dictionary for the result 
+        rolls = {}    # create a dictionary for the possible sum of the 2 dice rolls from 2-12 
+        for k in range(2, 13):
+            rolls[k] = 0
+        doubles = 0
 
-		for k in range(num):
-			first = random.randint(1, 6)
-			second = random.randint(1, 6)
-			if first == second:
-					doubles+=1
-			rolls[first+second]+=1
+        for k in range(num):
+            first = random.randint(1, 6)   # generate random numbers between 1 to 6
+            second = random.randint(1, 6)
+            sum = first + second			
+            if first == second:
+                doubles+=1  # increment doubles if same number rolled on both dices
+            rolls[sum]+=1
 
-		for k in rolls:
-			#print('%d - %d %f%%' %(k, rolls[k], float(rolls[k])/float(num)*100))
-			dict2 = {k:rolls[k]}		
-			result.update(dict2)		
+        for k in rolls:
+            dict2 = {k:rolls[k]}		
+            result.update(dict2)		
 
-		#print('Doubles - %d - %f%%' %(doubles, float(doubles)/float(num)))
-		dict3 = {'Doubles':doubles}	
-		result.update(dict3)	
-		return json.dumps(result)
+        dict3 = {'Doubles':doubles}	
+        result.update(dict3)	
+        return json.dumps(result)
     else:
         return render_template('userInput.html')	
 	
